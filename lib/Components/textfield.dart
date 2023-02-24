85809936c2dd27df03f0ced91e528e277f0d3845
Yash Jaiswal <bitorsic@protonmail.com>
2023-02-24 16:56:07 +0530

    Day 2

--- lib/Components/textfield.dart
+++ lib/Components/textfield.dart
@@ -7,23 +7,26 @@ import '../Constants.dart' as constant;
 class textfield extends StatelessWidget {
   final String text;
   final bool isPassword;
+
+  final Function(String) onChanged;
   const textfield({
-    super.key, required this.text, required this.isPassword,
+    super.key, required this.text, required this.isPassword, required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(20.0),
       child: TextField(
+        onChanged: onChanged,
         style: TextStyle(color: constant.textSecondary),
         obscureText: isPassword,
         decoration: InputDecoration(
           hintText: text,
           hintStyle: TextStyle(
             color: constant.textSecondary,
-            // fontSize: 20.0,
-            // fontWeight: FontWeight.w500,
+            fontSize: 15.0,
+            fontWeight: FontWeight.w200,
             // fontStyle: FontStyle.italic,
           ),
           enabledBorder: OutlineInputBorder(
--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gdsc/Components/textfield.dart';
 import '../Components/textfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'mainscreen.dart';
 import '../Constants.dart' as constant;
 
 
@@ -13,8 +15,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late String email, password;
+  int myVar = 0;
+
   @override
   Widget build(BuildContext context) {
+    final _auth = FirebaseAuth.instance;
     return Scaffold(
       backgroundColor: constant.textPrimary,
       body: Column(
@@ -22,29 +28,52 @@ class _LoginScreenState extends State<LoginScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(child: Container()),
-          Image.network('https://cdn.pixabay.com/photo/2017/12/26/21/19/circle-3041437__480.jpg'),
-          textfield(text: 'Email', isPassword: false),
-          textfield(text: 'Password', isPassword: true),
+          Container(
+              height: 150,
+              child: Image.network('https://cdn.pixabay.com/photo/2017/12/26/21/19/circle-3041437__480.jpg')),
+          textfield(
+            text: 'Email',
+            isPassword: false,
+            onChanged: (value) { email = value; },),
+          textfield(
+            text: 'Password',
+            isPassword: true,
+            onChanged: (value) { password = value; },),
           ClipRRect(
             borderRadius: BorderRadius.circular(30),
             child: Container(
               width: 100,
               color: constant.textSecondary,
               child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    side: BorderSide(
-                      color: constant.textSecondary,
-                      width: 2,
-                    )
-                  ),
-                ),
-                onPressed: (){},
+                // style: TextButton.styleFrom(
+                //   shape: RoundedRectangleBorder(
+                //     borderRadius: BorderRadius.circular(30),
+                //     side: BorderSide(
+                //       color: constant.textSecondary,
+                //       width: 2,
+                //     )
+                //   ),
+                // ),
+                onPressed: () async {
+                  try {
+                    final newUser = await _auth.signInWithEmailAndPassword(
+                      email: email, password: password
+                    );
+
+                    // print(newUser.toString());
+
+                    if (newUser.user != null && myVar != 0) {
+                      Navigator.pushReplacement(
+                          context, MaterialPageRoute(builder: (context) => MainScreen()));
+                    }
+                  } catch (e) {
+                    debugPrint('&e');
+                  }
+                },
                 child: Text(
                   'Login',
                   style: TextStyle(
-                    fontSize: 20.0,
+                    fontSize: 18.0,
                     color: constant.textPrimary,
                   ),
                 ),
